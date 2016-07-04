f2b9af31a5510a2c8e88b8266c08d94db5dd23d5
Hua Zhang <zhanghuanlp@gmail.com>
2016-07-04 10:09:21 +0800

    create charseg.py

---
+++ charseg.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#-*-coding = utf-8-*-
+# split line into char list
+import sys
+
+def charseg(str):
+        str = unicode(str,"utf-8")
+        l = list(str)
+        m = ""
+        for i in range(len(l)):
+                if m != "":
+                        m = m + " " + l[i]
+                else:
+                        m = l[i]
+        return m.encode("utf-8")
+
+for line in sys.stdin:
+        segresult = charseg(line.strip());
+        print segresult;
