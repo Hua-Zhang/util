e25141d55083e8bfe01781abd7e40f04671fbc76
Hua Zhang <zhanghuanlp@gmail.com>
2017-05-16 14:36:50 +0800

    update the interface

--- code_trans.py
+++ code_trans.py
@@ -1,18 +1,29 @@
 #!/usr/bin/env python
-#-*-coding = utf-8-*-
+#-*- coding:UTF-8 -*-
 #A character encoding conversion
 import sys
 
 text_type = unicode
 
 def strdecode(sentence):
-        if not isinstance(sentence, text_type):
-                try:
-                        sentence = sentence.decode('utf-8')
-                except UnicodeDecodeError:
-                        sentence = sentence.decode('gbk', 'ignore')
-                return sentence
+    if not isinstance(sentence, text_type):
+        try:
+            sentence = sentence.decode('utf-8')
+        except UnicodeDecodeError:
+            sentence = sentence.decode('gbk', 'ignore')
+    return sentence
 
-for line in sys.stdin:
+def trans(out_code):
+    for line in sys.stdin:
         trans = strdecode(line).strip()
-        print trans.encode("utf-8")
+        # out_code("utf-8" || 'gbk')
+        print trans.encode(out_code)
+
+if __name__=="__main__":
+    out_code = "utf-8"
+    if len(sys.argv) == 2:
+        if sys.argv[1].strip() == "-h":
+            print "Usage: code_trans out_code(utf-8||gbk||..., default=utf-8)"
+            exit(1)
+        out_code = sys.argv[1].strip()
+    trans(out_code)
