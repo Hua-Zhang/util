e5f3ddf833de176d376340e8a2d84df829ba261f
Hua Zhang <zhanghuanlp@gmail.com>
2018-04-16 16:13:32 +0800

    Update simpleHttpServer.sh

--- simpleHttpServer.sh
+++ simpleHttpServer.sh
@@ -2,10 +2,18 @@
 
 port=44407
 
+#如果ifconfig没有预支持
+#ifconfig='/sbin/ifconfig'
+
 if [ $# == 1 ]
 then
   port=$1
 fi
 
 ifconfig | grep "Bcast" | awk '{split($2, a, ":"); print a[2]":""'$port'"}'
+#如果ifconfig没有预支持
+#$ifconfig | grep "Bcast" | awk '{split($2, a, ":"); print a[2]":""'$port'"}'
+
 python -m SimpleHTTPServer $port
+#Python3
+#python -m http.server $port
